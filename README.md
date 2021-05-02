5a5811f6392e53e602cc81df5be606a3a03426af
Lynn E Demarest <lynn@lynndemarest.com>
2021-05-01 22:46:45 -0400

    Dropped changes added

--- README.md
+++ README.md
@@ -1,4 +1,37 @@
 *Nodejs API using Express*
 
-This little project creates an API that allows the user to manipulate files and folders in a public folder location, FileManData. 
+This little project creates an node.js API that illustrates several node.js/Express features. 
+
+
+1. app.use("/api/courses", require("./routes/api/courses.js"));
+
+Endpoint to illustrate parameters. 
+
+2. app.use("/api/files", require("./routes/api/fileman.js"));
+
+Endpoint to exercise the node.js fs class. Illustrates the use of async/await, promises, and callbacks versions of fs.
+
+3. app.use("/api/mssql", require("./routes/api/mssql.js"));
+
+Endpoint hits SQL Server using mssql. 
+
+4. 
+
+Features include: 
+
+* @hapi/joi for form validation
+* bcryptjs to encode passwords
+* dotenv to facilitate secret .env settings
+* jsonwebtoken to encode and decode JWT tokens
+* mongoose to help with mongodb
+* mssql to read MS Sql Server database 
+* middleware authentication to protect a given endpoint 
+
+
+
+
+
+
+
+Thank you to Dev Ed, whose video helped me a great deal: https://www.youtube.com/watch?v=2jqok-WgelI&t=1502s
 
--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const express = require("express");
 const path = require("path");
-
+const dotenv = require("dotenv");
+const { DH_NOT_SUITABLE_GENERATOR } = require("constants");
 const app = express();
+//const posts = require("mongoose");
+dotenv.config();
 
 // Add middleware
 //
-//
+// .json parses incoming json data 
+// If you remove .json() the controller endpoint is not found! 
+// 
 app.use(express.json());
+
 
 // All HTML files in 'public' folder should be served 
 // Note: You must include the .html, i.e., About.html 
@@ -15,55 +21,15 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Inject the courses middleware into the pipeline.
 //
+app.use("/api/user", require("./routes/api/auth.js"));
+
 app.use("/api/courses", require("./routes/api/courses.js"));
 
 app.use("/api/files", require("./routes/api/fileman.js"));
 
 app.use("/api/mssql", require("./routes/api/mssql.js"));
 
-// let courses = [
-// { id: 1, name: "Course1"},
-// { id: 2, name: "Course2"},
-// { id: 3, name: "Course3"}
-// ];
-
-// // app.get, post, put, delete
-
-// app.get("/", (req, res) => {
-//     res.send("Hello World!!!");
-// });
-
-// app.get('/api/courses', (req, res) => {
-//     // either one works 
-//     //res.json(courses);
-//     res.send(courses);
-// })
-
-// app.get('/api/courses/:id', (req, res) =>
-// {
-//     res.send(req.params.id);
-//     // req.query has querystring args
-// })
-
-// // new course
-// app.post('/api/courses', (req, res) =>
-// {
-//     const schema = Joi.object({
-//         name: Joi.string().min(3).required()
-//     });
-//     const { error }  = schema.validate(req.body);
-//     if (error){
-//         res.status(400).send(error.details[0].message);
-//         return;
-//     }
-
-//     const course = { id: courses.length+1, 
-//                      name: req.body.name };
-//     console.log(course);
-//     courses.push(course);
-//     res.send(course);
-// })
-
+app.use("/api/posts", require("./routes/api/posts.js"));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
